4c53905328b14b5191a5463ae4e297cc2d0cc81d
Violetta Rusovich <violettarusovich@MacBook-Pro-Violetta.local>
2024-12-09 18:17:52 +0300

    Переписаный компонент App.js без использования JSX

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,29 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
 
 function App() {
   const currentYear = new Date().getFullYear()
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* Декларативный стиль */}
-        <p>Current Year: {currentYear}</p> {/* Декларативный стиль */}
-      </header>
-    </div>
+  const header = React.createElement('header', { className: 'App-header' },
+    React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+    React.createElement('p', null, 'Edit ', React.createElement('code', null, 'src/App.js'), ' and save to reload.'),
+    React.createElement('a', {
+      className: 'App-link',
+      href: 'https://reactjs.org',
+      target: '_blank',
+      rel: 'noopener noreferrer'
+    }, 'Learn React'),
+    React.createElement('p', null, 'Current Year: ', currentYear)
   );
+
+  const app = React.createElement('div', { className: 'App' }, header);
+
+  return app;
 }
 
 export default App;
